b120d2fc6d746def08df34b08fe59e0865cd52df
patelr124 <70537858+patelr124@users.noreply.github.com>
2022-03-08 19:42:53 -0500

    working option 1/2/3 BOOYAH

--- Project 2/sudoku.c
+++ Project 2/sudoku.c
@@ -158,6 +158,61 @@ void *validSquares(void* params){
     pthread_exit(0);
 }
 
+int validColsProcess(){ /* checks validity of all columns in process */
+    for(int i = 0; i < 9; i++){
+        /* Reset flags for each col */
+        int flagCol[9] = {0,0,0,0,0,0,0,0,0}; 
+        for(int j = 0; j< 9; j++){
+            if(flagCol[sudoku[j][i]-1] == 0){
+                flagCol[sudoku[j][i]-1] = 1;
+            }
+            else{
+               return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int validRowsProcess(){ /* checks validity of all rows in process */
+    for(int i = 0; i < 9; i++){
+        /* Reset flags for each row */
+        int flagRow[9] = {0,0,0,0,0,0,0,0,0}; 
+        for(int j = 0; j< 9; j++){
+            if(flagRow[sudoku[i][j]-1] == 0){
+                flagRow[sudoku[i][j]-1] = 1;
+            }
+            else{
+               return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+/* checks all 9 square to see if all squares are valid */
+int validSquaresProcess(void* params){
+    int temp = 0;
+    parameters *param = (parameters*) params;
+    int cornerRow = param->row;
+    int cornerCol = param->col;
+
+    int flagSquare[9] = {0,0,0,0,0,0,0,0,0}; 
+    for(int i = cornerRow; i < cornerRow+3; i++){
+        for(int j = cornerCol; j< cornerCol + 3; j++){ /* goes through small square from left corner index */
+            temp = sudoku[i][j]; /* pull individual value */
+ 
+            if(temp>0 && temp<10 && (flagSquare[temp-1] == 0)){ /* if doesnt exist, change flag to 1 */
+                flagSquare[temp-1] = 1;
+            }
+            else{
+               
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
 
 int main(int argc, char** argv) {
     /* Reading a sample file */ /* NEED TO ADD A USER INPUT HERE! */
@@ -183,18 +238,27 @@ int main(int argc, char** argv) {
             nThreads = 27;
             break;
         case 3:
-            nThreads = 0;
+            nThreads = 11;
             break;
         default:
             nThreads = 0;
     }
 
+    /* for the threads cases */
     pthread_t threads[nThreads];
-    /*int shm_fd;
+    int thread_num = 0;
+
+    /* for the process case */
+    int shm_fd;
     shm_fd = shm_open("VALID", O_CREAT | O_RDWR, 0666);
     ftruncate(shm_fd, 4096);
-    int *ptr = mmap(0, 4096, PROT_WRITE, MAP_SHARED, shm_fd, 0);*/
-    int thread_num = 0;
+    int *ptr = mmap(0, 4096, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    int result = 0;
+
+    /* clock things */
+    clock_t t;
+    t = clock();
+
     switch(input){
         case 1:
             /* Option 1: 11 thread, 9 for each square, 1 for all rows and 1 for all columns */
@@ -231,17 +295,73 @@ int main(int argc, char** argv) {
                 pthread_create(&threads[thread_num++], NULL, validCol, data2);
             }
             break;
+        case 3:
+            /* Option 3: 1 process for all rows, 1 process for all columns, 1 process per square */
+            thread_num = 0;
+
+            pid_t rows = fork();
+            if (rows == 0){
+                result = validColsProcess();
+                if(result != 1) /* valid columns */
+                    *ptr = result;
+                
+                _Exit(EXIT_SUCCESS);
+            } else {
+                int waitFlag;
+                waitpid(rows, &waitFlag, 0);
+            }
+
+            pid_t columns = fork();
+            if (columns == 0){
+                result = validColsProcess();
+                if(result != 1) /* valid columns */
+                    *ptr = result;
+                
+                _Exit(EXIT_SUCCESS);
+            } else {
+                int waitFlag;
+                waitpid(columns, &waitFlag, 0);
+            }
+
+            for(int i = 0; i<9; i++){
+                for(int j = 0; j<9; j++){
+                    if(i%3 == 0 && j%3==0){
+                        pid_t square = fork();
+                        if(square == 0){
+                            parameters *data = (parameters*) malloc(sizeof(parameters));
+                            data->row = i;
+                            data->col = j;
+                            result = validSquaresProcess(data);
+
+                            if(result != 1) /* valid square */
+                                *ptr = result;
+                            
+                            _Exit(EXIT_SUCCESS);
+                        } else {
+                            int waitFlag;
+                            waitpid(square, &waitFlag, 0);
+                        }
+                    }
+                }
+            }
+            break;
     }
     
+    validFlag = *ptr;
+    shm_unlink("VALID");
+    t = clock() - t;
+    double time_tot = ((double)t/CLOCKS_PER_SEC);
+
+
     for (int i = 0; i<nThreads; i++){
         pthread_join(threads[i], NULL);
     }
 
     
-    if(validFlag == 0){ /* valid when flag = 1 */
-        printf("SOLUTION: NO\n");
+    if(validFlag == 0 ){ /* valid when flag = 1 */
+        printf("SOLUTION: NO (%f seconds)\n", time_tot);
     } else
-        printf("SOLUTION: YES\n");
+        printf("SOLUTION: YES (%f seconds)\n", time_tot);
 
     return 0;
 }
